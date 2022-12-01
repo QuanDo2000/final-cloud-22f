91c9235aa48a47020a5de1801a16e4f858b2a108
Quan Do <minhquand3@gmail.com>
2022-12-01 05:48:22 +0000

    Add MySQL scripts

--- README.md
+++ README.md
@@ -16,5 +16,9 @@ Open [http://localhost:3000](http://localhost:3000) with your browser to see the
 
 ## References
 
-<https://github.com/vercel/next.js/tree/canary/examples/with-iron-session>
-<https://github.com/mui/material-ui/blob/v5.10.16/docs/data/material/getting-started/templates/sign-in/SignIn.tsx>
+- Login Example <https://github.com/vercel/next.js/tree/canary/examples/with-iron-session>
+- Signin template from MUI <https://github.com/mui/material-ui/blob/v5.10.16/docs/data/material/getting-started/templates/sign-in/SignIn.tsx>
+- Deploy options <https://stackoverflow.com/questions/68470162/how-to-archive-files-in-artifact-for-github-workflow-actions-in-order-to-fix-thi>
+- NextJS with MySQL <https://github.com/vercel/next.js/tree/canary/examples/with-mysql>
+- Prisma with NextJS in TypeScript <https://github.com/prisma/prisma-examples/tree/latest/typescript/rest-nextjs-api-routes>
+- CSV Reading <https://blog.tericcabrel.com/read-csv-node-typescript/>
---
+++ mysql/seed.js
@@ -0,0 +1,127 @@
+const fs = require('fs');
+const fastcsv = require('fast-csv');
+const mysql = require('mysql');
+
+const processData = (filePath, query) => {
+  return new Promise((resolve) => {
+    console.log('Processing data from ' + filePath + ' ...');
+
+    let stream = fs.createReadStream(filePath);
+    let csvData = [];
+    let csvStream = fastcsv
+      .parse({ trim: true })
+      .on('data', (data) => {
+        csvData.push(data);
+      })
+      .on('end', () => {
+        csvData.shift();
+
+        conn.query(query, [csvData], (err, res) => {
+          console.log(err || res);
+        });
+
+        resolve();
+      });
+
+    stream.pipe(csvStream);
+  });
+};
+
+const insertData = async () => {
+  let filePath = './400_households.csv';
+  let query =
+    'INSERT INTO household(Hshd_num, Loyalty_flag, Age_range, Marital_status, Income_range, Homeowner_desc, Hshd_composition, Hshd_size, Children) VALUES ?';
+  await processData(filePath, query);
+  filePath = './400_transactions.csv';
+  query =
+    'INSERT INTO transaction(Hshd_num, Basket_num, Date, Product_num, Spend, Units, Store_region, Week_num, Year) VALUES ?';
+  await processData(filePath, query);
+  filePath = './400_products.csv';
+  query =
+    'INSERT INTO product(Product_num, Department, Commodity, Brand_type, Natural_organic_flag) VALUES ?';
+  await processData(filePath, query);
+  return;
+};
+
+const initTables = () => {
+  conn.query('DROP TABLE IF EXISTS household;', (err) => {
+    if (err) {
+      console.log('Error: ', err);
+      return;
+    }
+    console.log('Dropped household table if existed.');
+  });
+
+  let createStatement =
+    'CREATE TABLE household(Hshd_num INT NOT NULL, Loyalty_flag VARCHAR(5), Age_range VARCHAR(20), Marital_status VARCHAR(20), Income_range VARCHAR(20), Homeowner_desc VARCHAR(50), Hshd_composition VARCHAR(20), Hshd_size VARCHAR(5), Children VARCHAR(5));';
+
+  conn.query(createStatement, (err) => {
+    if (err) {
+      console.log('Error: ', err);
+      return;
+    }
+    console.log('Created household table.');
+  });
+
+  conn.query('DROP TABLE IF EXISTS transaction;', (err) => {
+    if (err) {
+      console.log('Error: ', err);
+      return;
+    }
+    console.log('Dropped transaction table if existed.');
+  });
+
+  createStatement =
+    'CREATE TABLE transaction(Hshd_num INT NOT NULL, Basket_num INT NOT NULL, Date VARCHAR(20), Product_num INT NOT NULL, Spend DECIMAL(5, 2), Units INT, Store_region VARCHAR(20), Week_num INT, Year INT);';
+
+  conn.query(createStatement, (err) => {
+    if (err) {
+      console.log('Error: ', err);
+      return;
+    }
+    console.log('Created transaction table.');
+  });
+
+  conn.query('DROP TABLE IF EXISTS product;', (err) => {
+    if (err) {
+      console.log('Error: ', err);
+      return;
+    }
+    console.log('Dropped product table if existed.');
+  });
+
+  createStatement =
+    'CREATE TABLE product(Product_num INT NOT NULL, Department VARCHAR(20), Commodity VARCHAR(50), Brand_type VARCHAR(20), Natural_organic_flag VARCHAR(5));';
+
+  conn.query(createStatement, (err) => {
+    if (err) {
+      console.log('Error: ', err);
+      return;
+    }
+    console.log('Created product table.');
+  });
+
+  console.log('Finished initialization.');
+};
+
+let conn = mysql.createConnection({
+  host: 'cloud-database-server.mysql.database.azure.com',
+  user: 'azureuser',
+  password: 'myPassword9200',
+  database: 'data',
+  port: 3306,
+  ssl: { ca: fs.readFileSync('DigiCertGlobalRootCA.crt.pem') },
+});
+
+initTables();
+
+insertData().then(() => {
+  conn.end((err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log('Finished.');
+  });
+});
