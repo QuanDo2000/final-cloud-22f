5f7cb865c024d433fed435fe09d5363a3d57d144
Quan Do <minhquand3@gmail.com>
2022-12-01 13:50:21 -0600

    Remove await from API function

--- pages/api/pull/[id].ts
+++ pages/api/pull/[id].ts
@@ -8,12 +8,18 @@ export default function pullHandler(req: NextApiRequest, res: NextApiResponse) {
 
   switch (method) {
     case 'GET':
-      const queryResult = await executeQuery(query, values: [id]);
-      const data = JSON.parse(JSON.stringify(queryResult));
-      res.status(200).json({ data })
+      executeQuery({ query, values: [id] })
+        .then((res) => {
+          const queryResult = res;
+          const data = JSON.parse(JSON.stringify(queryResult));
+          res.status(200).json({ data });
+        })
+        .catch((err) => {
+          console.error(err);
+        });
       break;
     default:
-      res.setHeader('Allow', ['GET'])
-      res.status(405).end(`Method ${method} Not Allowed`)
+      res.setHeader('Allow', ['GET']);
+      res.status(405).end(`Method ${method} Not Allowed`);
   }
 }
